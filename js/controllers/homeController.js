377ef0e7752d2df81c9db5287df575fad17996da
Chris <christoal@outlook.com>
2016-10-27 20:52:37 +0800

    new logout function and logout html update

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -2,6 +2,6 @@ angular.module('RouteControllers', [])
 
     .controller('HomeController', function($scope) {
         $scope.title = "Welcome To Angular Todo!";
-    })
+    });
 
    
--- js/controllers/logoutController.js
+++ js/controllers/logoutController.js
@@ -4,12 +4,21 @@ angular.module('RouteControllers', [])
     .controller('LogOutController', function($scope, $location, logOutAPIService, store) {
         var url = 'https://morning-castle-91468.herokuapp.com/';
 
-        $scope.authToken = store.remove('authToken');
-        $scope.username = store.remove('username');
-        
+        $scope.logoutMessage = "Do you really want to log out?"
+       
+        $scope.logout = function() {        
 
-        logOutAPIService.logout(url + 'accounts/api-token-auth/' + $scope.authToken + $scope.username)
-    })
+        logOutAPIService.logout(url + 'accounts/api-token-auth/', $scope.username, store.get('authToken')).then(function(results) {
+    
+        	$scope.authToken = store.remove('authToken');
+        	$scope.username = store.remove('username');
+        }.catch(function(err) {
+        	console.log(err)
+        })
+    });
+}
+    
+    
 
 
 
